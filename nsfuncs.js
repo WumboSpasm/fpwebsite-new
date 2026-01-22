5001fb81e6d78ff107e6b139513cf6a24f2c05ef
Wumbo <58399748+WumboSpasm@users.noreply.github.com>
2026-01-22 17:13:35 -0500

    nvm

--- nsfuncs.js
+++ nsfuncs.js
@@ -361,45 +361,6 @@ export const namespaceFunctions = {
 			})),
 		};
 	},
-	'play': async (url, lang, defs) => {
-		// Check if an ID has been supplied and if it is properly formatted
-		const id = url.searchParams.get('id');
-		if (id === null || !idExp.test(id))
-			throw new utils.BadRequestError();
-
-		// Fetch the entry, or display an error if it doesn't exist
-		const entry = await fp.findGame(id);
-		if (entry === null)
-			throw new utils.NotFoundError();
-
-		let gameZip, launchCommand;
-		if (entry.gameData.length > 0) {
-			// If the game is zipped, return the path and launch command of the latest game data
-			const sortedGameData = entry.gameData.toSorted((a, b) => new Date(b.dateAdded).getTime() - new Date(a.dateAdded).getTime());
-			const newestGameData = sortedGameData[0];
-			gameZip = `${config.zipServer}/${entry.id}-${new Date(newestGameData.dateAdded).getTime()}.zip`;
-			launchCommand = newestGameData.launchCommand;
-		}
-		else {
-			// If the game is not zipped, return its legacy launch command
-			gameZip = '';
-			launchCommand = entry.legacyLaunchCommand;
-		}
-
-		// Remove any prefixes from the launch command
-		const httpIndex = launchCommand.indexOf('http://');
-		if (httpIndex != 0) launchCommand = launchCommand.substring(httpIndex);
-
-		// Build entry player HTML
-		const title = utils.sanitizeInject(entry.title);
-		return {
-			Title: title,
-			Header: title,
-			legacyServer: config.legacyServer,
-			gameZip: gameZip,
-			launchCommand: launchCommand,
-		};
-	},
 	'search-info': (_, lang) => {
 		// Get search page translation
 		const translation = Object.assign({},
